404078cd239ba2b888860ddcd312a605ea8a2e31
AlessP93 <alespiscino@libero.it>
2022-05-30 15:30:08 +0200

    errore prentesi

--- main.js
+++ main.js
@@ -2,8 +2,48 @@
 // Bonus
 // Far comparire gli indirizzi email solamente quando sono stati tutti generati.
 
-
-
+const app = new Vue({
+    el: '#app',
+    data: {
+        emails: [
+            {
+                "response":"mariorossi@hotmail.com"
+            },
+            {
+                "response":"federicogialli@hotmail.com"
+            },
+            {
+                "response":"luciano1@gmail.com"
+            },
+            {
+                "response":"luisa99@hotmail.com"
+            },
+            {
+                "response":"alesverdi@hotmail.com"
+            },
+            {
+                "response":"marco55@libero.com"
+            },
+            {
+                "response":"pasquale@hotmail.com"
+            },
+            {
+                "response":"maria12@hotmail.com"
+            },
+            {
+                "response":"alessiogialli@yahoo.com"
+            },
+            {
+                "response":"franco100@hotmail.com"
+            },
+        ]
+    },
+    created() {
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((res) => {
+            this.emails = res.success.response;
+        })
+});
 
 
 
