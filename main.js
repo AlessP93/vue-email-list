a310886ad84f402ce7e550f8550afd738eebdbf0
AlessP93 <alespiscino@libero.it>
2022-05-30 16:22:23 +0200

    fine esercizio

--- main.js
+++ main.js
@@ -1,52 +1,17 @@
 // generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-// Bonus
-// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
-
 const app = new Vue({
     el: '#app',
     data: {
-        emails: [
-            {
-                "response":"mariorossi@hotmail.com"
-            },
-            {
-                "response":"federicogialli@hotmail.com"
-            },
-            {
-                "response":"luciano1@gmail.com"
-            },
-            {
-                "response":"luisa99@hotmail.com"
-            },
-            {
-                "response":"alesverdi@hotmail.com"
-            },
-            {
-                "response":"marco55@libero.com"
-            },
-            {
-                "response":"pasquale@hotmail.com"
-            },
-            {
-                "response":"maria12@hotmail.com"
-            },
-            {
-                "response":"alessiogialli@yahoo.com"
-            },
-            {
-                "response":"franco100@hotmail.com"
-            },
-        ]
+      emails: [],
     },
     created() {
-        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
-        .then((res) => {
-            this.emails = res.success.response;
-        })
+        for (let i = 0; i < 10; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            .then((res) => {
+                this.emails.push(res.data.response)
+            })
+        }
+    }
 });
-
-
-
-
 
 
