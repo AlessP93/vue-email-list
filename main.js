33bd5e42f264e66a7d5ebe32b778161d5daf82f4
AlessP93 <alespiscino@libero.it>
2022-05-30 16:26:08 +0200

    aggiunti commenti

--- main.js
+++ main.js
@@ -2,12 +2,18 @@
 const app = new Vue({
     el: '#app',
     data: {
+    //creo un array vuoto di email
       emails: [],
     },
+    //chiamo la funzione created
     created() {
+        //ciclo per massimo 10 volte la lista di mail
         for (let i = 0; i < 10; i++) {
+            //collego axios 
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+            //arrow function per richiamare il this.
             .then((res) => {
+                //pusho le mail dentro il mio array vuoto
                 this.emails.push(res.data.response)
             })
         }
